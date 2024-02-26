96182b080f9ddc25c92aa4e8086fa702f5d5a438
seseo123456789 <seseo59@gmail.com>
2024-02-26 16:54:10 +0900

    score01

--- src/main/java/com/green/Team3/test/controller/TestController.java
+++ src/main/java/com/green/Team3/test/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.green.Team3.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "content/test/pr_list";
+    }
 }
