299ba56254086cc1b038de6ab45e0df21a2a67a4
thdals12345 <thdals12345@naver.com>
2024-02-27 17:21:16 +0900

    역할별 로그인 구현

--- src/main/java/com/green/Team3/member/controller/MemberController.java
+++ src/main/java/com/green/Team3/member/controller/MemberController.java
@@ -24,16 +24,12 @@ public class MemberController {
     //회원가입
     @PostMapping("/join")
     public String join(MemberVO memberVO){
-
         //연락처 세팅 (010,0000, 0000 -> 010-0000-0000) 변환 후 set(저장)
         memberVO.setMemberTel(memberVO.getMemberTel().replace(",", "-"));
-
         //이메일 세팅
         memberVO.setMemberEmail(memberVO.getMemberEmail().replace(",", ""));
-
         //회원가입 쿼리
         memberService.join(memberVO);
-
         return "redirect:/";
     }
 
@@ -41,11 +37,35 @@ public class MemberController {
     @PostMapping("/login")
     public String login(MemberVO memberVO, HttpSession session){
         MemberVO loginInfo = memberService.login(memberVO);
+        System.out.println(loginInfo.getMemberRoll());
         //로그인 성공
         if(loginInfo != null){
             session.setAttribute("loginInfo", loginInfo);
+            //memberRoll == 1 (학생)
+            if (loginInfo.getMemberRoll() == 1){
+                return "content/student/stu_list";
+            }
+            //memberRoll == 2 (강사)
+            else if (loginInfo.getMemberRoll() == 2) {
+
+                return "content/teacher/tea_list";
+            }
+            //memberRoll == 3 (관리자)
+            else {
+                return "content/admin/admin_list";
+            }
         }
-        return "content/member/login_result";
+        System.out.println("일치하는 회원 정보가 없습니다.");
+        session.setAttribute("loginInfo", null);
+        System.out.println(memberVO);
+
+        return "redirect:/";
+
+//        if(loginInfo != null){
+//            session.setAttribute("loginInfo", loginInfo);
+//        }
+//        return "content/member/login_result";
+
     }
 
 
