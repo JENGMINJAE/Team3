9f6b47fa86ff4133966938344cd48cdcc61edb74
JENGMINJAE <alswo990602@naver.com>
2024-03-04 16:42:00 +0900

    beforeFix

--- src/main/java/com/green/Team3/learn/controller/HomeworkController.java
+++ src/main/java/com/green/Team3/learn/controller/HomeworkController.java
@@ -1,11 +1,18 @@
 package com.green.Team3.learn.controller;
 
 import com.green.Team3.learn.service.HomeworkServiceImpl;
+import com.green.Team3.learn.vo.HomeworkVO;
+import com.green.Team3.learn.vo.LearnVO;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/homework")
@@ -14,8 +21,14 @@ public class HomeworkController {
     private HomeworkServiceImpl homeworkService;
 
     @GetMapping("/addHomework")
-    private String addHomework(){
-
+    private String addHomework(Model model){
+        String memberId = "t";
+        model.addAttribute("classList",homeworkService.selectClassByThisTeacher(memberId));
         return "/content/teacher/add_homework";
+    }
+    @PostMapping("/addHomeworkResult")
+    private String addHomeworkResult(HomeworkVO homeworkVO){
+        homeworkService.homeworkAdd(homeworkVO);
+        return "/";
     }
 }
--- src/main/java/com/green/Team3/learn/service/HomeworkServiceImpl.java
+++ src/main/java/com/green/Team3/learn/service/HomeworkServiceImpl.java
@@ -1,11 +1,26 @@
 package com.green.Team3.learn.service;
 
+import com.green.Team3.cls.vo.ClsVO;
+import com.green.Team3.learn.vo.HomeworkVO;
+import com.green.Team3.learn.vo.LearnVO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("homeworkService")
 public class HomeworkServiceImpl implements HomeworkService{
     @Autowired
     private SqlSessionTemplate sqlSession;
+
+    @Override
+    public List<ClsVO> selectClassByThisTeacher(String memberId) {
+        return sqlSession.selectList("learnMapper.selectClassByThisTeacher",memberId);
+    }
+
+    @Override
+    public void homeworkAdd(HomeworkVO homeworkVO) {
+        sqlSession.insert("learnMapper.homeworkAdd",homeworkVO);
+    }
 }
