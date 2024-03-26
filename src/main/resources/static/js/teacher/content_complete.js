d24fe920d846610c4fa29f93a6c0362ec56aeb3c
JENGMINJAE <alswo990602@naver.com>
2024-03-26 15:14:34 +0900

    consult 88%

--- src/main/resources/static/js/teacher/content_complete.js
+++ src/main/resources/static/js/teacher/content_complete.js
@@ -1,11 +1,29 @@
 function look(thisDetail,content){
+    const CCB = document.getElementById('CCB');
+    const CDB = document.getElementById('CDB');
+    const CCHB = document.getElementById('CCHB');
+    const CCCB = document.getElementById('CCCB');
+    
+    if(CCB.style.display == 'none') {
+        CCB.style.display = 'inline-block';
+	    CDB.style.display = 'inline-block';
+        CCHB.style.display = 'none';
+        CCCB.style.display = 'none';
+
+	}
     const className = thisDetail.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
     const stuName = thisDetail.parentElement.previousElementSibling.previousElementSibling.textContent;
     const cstDate = thisDetail.parentElement.previousElementSibling.textContent;
+    const cstNum =  thisDetail.previousElementSibling.value;
     const plusHere = document.querySelector(".here");
     let str = '';
     plusHere.innerHTML = '';
-    str+=`<table class="table" id="detail" style="text-align: center; vertical-align: middle;">
+    str+=`<table class="table table-striped table-bordered" id="detail" style="text-align: center; vertical-align: middle;">
+            <colgroup>
+                <col width="30%">
+                <col width="30%">
+                <col width="40%">
+            </colgroup>
             <thead>
                 <tr>
                     <td>소속</td>
@@ -26,13 +44,65 @@ function look(thisDetail,content){
                     str+=`</td>
                 </tr>
                 <tr>
-                    <td colspan="3">
+                    <td colspan="3" style="font-weight: bolder;">
+                        상담내용
+                    <input type="hidden" id="consultNum" value="`;
+                    str+= cstNum
+                    str+=`">
+                    <input type="hidden" id="content" value="`;
+                    str+= content;
+                    str+=`
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" id="cctd">
                         `;
                         str+=content
                         str+=`
                     </td>
                 </tr>
             </tbody>
-        </table>`;
+        </table>
+        `
+        
+        ;
     plusHere.insertAdjacentHTML("afterbegin",str)
+}
+
+function crystalConsultContent(){
+    const selected_td = document.querySelector("#cctd");
+    const consultNum = document.getElementById('consultNum').value;
+    const CCB = document.getElementById('CCB');
+    const CCHB = document.getElementById('CCHB');
+    const CCCB = document.getElementById('CCCB');
+    const CDB = document.getElementById('CDB');
+    if(CCB.style.display !== 'none') {
+	    CCB.style.display = 'none';
+	    CDB.style.display = 'none';
+	}
+    if(CCHB.style.display == 'none') {
+	    CCHB.style.display = 'inline-block';
+	    CCCB.style.display = 'inline-block';
+	}
+    selected_td.innerHTML="";
+    let str =``;
+    str+=`<form action="/consult/addConsultContent" method="post" id="newContentCrystal">
+                <input type="hidden" name="consultNum" value="`
+                str+=consultNum;
+                str+=`">
+                <textarea class="form-control" name="consultContent" rows="10"></textarea>
+            </form>`;
+    selected_td.insertAdjacentHTML("afterbegin",str)
+}
+
+
+
+function crystalContentOk(){
+    const sbm = document.querySelector('#newContentCrystal');
+    sbm.submit();
+}
+
+function crstalCancle(){
+    // const goBack =
+    
 }
